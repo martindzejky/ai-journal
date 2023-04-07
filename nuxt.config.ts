da04080bbcfe918644a6efbe6ccd88b923aabf23
Martin Jakubik <martindzejky@gmail.com>
2023-04-07 14:53:02 +0200

    configure vuefire and add modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,24 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
+export default {
+    modules: [
+        // used modules
+        '@vueuse/nuxt',
+        '@nuxtjs/tailwindcss',
+        '@pinia/nuxt',
+        'nuxt-vuefire',
+    ],
 
-})
+    // https://vuefire.vuejs.org/nuxt/getting-started.html
+    // https://github.com/chrisspiegl/test-nuxt-vuefire-ssr/blob/main/nuxt.config.ts
+    vuefire: {
+        auth: true,
+        config: {
+            apiKey: process.env.FIREBASE_API_KEY,
+            authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+            projectId: process.env.FIREBASE_PROJECT_ID,
+            storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+            messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+            appId: process.env.FIREBASE_APP_ID,
+        },
+    },
+};
