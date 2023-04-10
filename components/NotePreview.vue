f7d94d031e5f6eb8f507f4f281cd9f128dc2fa9d
Martin Jakubik <martindzejky@gmail.com>
2023-04-10 21:24:44 +0200

    display the date in list of notes

--- components/NotePreview.vue
+++ components/NotePreview.vue
@@ -4,6 +4,9 @@
         class="mx-6 md:mx-0 md:border-t md:last:border-b md:border-stone-200"
     >
         <article class="bg-white px-6 rounded-xl py-4 md:rounded-none">
+            <p class="mb-1 text-sm text-slate-400">
+                {{ date }}
+            </p>
             <Editor
                 class="line-clamp-3 text-ellipsis"
                 :model-value="note.content"
@@ -17,11 +20,21 @@
 <script setup lang="ts">
 import { Note } from '~/types/note';
 import { PropType } from '@vue/runtime-core';
+import { formatRelative } from 'date-fns';
+import { capitalize } from 'lodash-es';
 
-defineProps({
+const props = defineProps({
     note: {
         type: Object as PropType<Note>,
         required: true,
     },
+});
+
+const { note } = toRefs(props);
+
+const date = computed(() => {
+    const formatted = formatRelative(note.value.timestamp.toDate(), new Date());
+    const split = formatted.split(' at ');
+    return capitalize(split[0]);
 });
 </script>
