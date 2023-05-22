f3dcaea25274c225f3348437a33a7f5380d61768
Martin Jakubik <martindzejky@gmail.com>
2023-05-22 18:02:45 +0200

    update chromadb package

--- functions/src/chroma/chroma.ts
+++ functions/src/chroma/chroma.ts
@@ -1,51 +1,66 @@
 import { Note } from '../../../types/note';
 import { ChromaClient, OpenAIEmbeddingFunction } from 'chromadb';
 import { getNoteContentAsText } from '../get-note-content';
-import { QueryEmbeddingIncludeEnum } from 'chromadb/dist/main/generated';
 
 export class Chroma {
     private client = new ChromaClient();
-    private embedder = new OpenAIEmbeddingFunction(this.openAiApiKey);
+    private embedder = new OpenAIEmbeddingFunction({ openai_api_key: this.openAiApiKey });
 
     constructor(private openAiApiKey: string) {}
 
-    async query(prompt: string, owner: string, results = 4) {
+    async query(prompt: string, owner: string, results = 4): Promise<Array<string>> {
         const collection = await this.prepareCollection();
-        return await collection.query(undefined, results, { owner }, prompt, undefined, [
-            QueryEmbeddingIncludeEnum.Distances,
-        ]);
+
+        const result = await collection.query({
+            query_text: prompt,
+            where: { owner },
+            n_results: results,
+        });
+
+        return result.ids;
     }
 
     async createNote(note: Note) {
         const collection = await this.prepareCollection();
-        await collection.add(
-            note.id,
-            undefined,
-            {
-                owner: note.owner,
-            },
-            getNoteContentAsText(note),
-        );
+        await collection.add({
+            ids: [note.id],
+            metadatas: [{ owner: note.owner }],
+            documents: [getNoteContentAsText(note)],
+        });
+
+        console.log('PEEK:');
+        console.log(await collection.peek());
     }
 
     async updateNote(note: Note) {
         const collection = await this.prepareCollection();
-        await collection.update(
-            note.id,
-            undefined,
-            {
-                owner: note.owner,
-            },
-            getNoteContentAsText(note),
-        );
+
+        // first check if the note already exists in the collection and if not, create it
+        const existingNote = await collection.get({ ids: [note.id] });
+        console.log('existingNote', existingNote);
+
+        await collection.update({
+            ids: [note.id],
+            metadatas: [{ owner: note.owner }],
+            documents: [getNoteContentAsText(note)],
+        });
+
+        console.log('PEEK:');
+        console.log(await collection.peek());
     }
 
     async deleteNote(id: Note['id']) {
         const collection = await this.prepareCollection();
-        await collection.delete([id]);
+        await collection.delete({ ids: [id] });
+
+        console.log('PEEK:');
+        console.log(await collection.peek());
     }
 
     private async prepareCollection() {
-        return await this.client.getOrCreateCollection('notes', {}, this.embedder);
+        return await this.client.getOrCreateCollection({
+            name: 'notes',
+            embeddingFunction: this.embedder,
+        });
     }
 }
